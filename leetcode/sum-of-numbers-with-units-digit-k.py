ff04ee5db78ffbae6dbea4ac56acac0c24e64d82
Baso1st <abdulwanis1@gmail.com>
2022-06-22 12:51:37 -0400

    Adding another solution to the sum-of-numbers-with-units-digit-k problem (AKA coin change problem, AKA knapsack problem)

--- leetcode/sum-of-numbers-with-units-digit-k.py
+++ leetcode/sum-of-numbers-with-units-digit-k.py
@@ -1,5 +1,6 @@
-#2310. Sum of Numbers With Units Digit K
+#2310. Sum of Numbers With Units Digit K ===> AKA Coin Chnage AKA knapsack problem
 #https://leetcode.com/problems/sum-of-numbers-with-units-digit-k/submissions/
+#Two solutions are provided for this problem. There is a better one as well using dynamic programming bottom up approach
 
 class Solution:
     def minimumNumbers(self, num: int, k: int) -> int:
@@ -47,7 +48,45 @@ class Solution:
             
         return False
     
+#######################Below is top down approach using dynamic programming#####################
+class Solution:
+    def minimumNumbers(self, num: int, k: int) -> int:
+        if k == 0 and num % 10 != 0:
+            return -1         
+        all_ints = []
+        
+        i = k
+        while i <= num:
+            if i :
+                all_ints.append(i)
+            i += 10
 
+        memo = {}
+        memo[0] = 0
+        self.least_count(num, all_ints, memo)
+        if num in memo:
+            return memo[num] if memo[num] < float('inf') else -1
+        else:
+            return -1          
+
+    def least_count(self, num, arr, memo):
+
+        if num == 0:
+            return 0
+
+        if num in memo:
+            return memo[num]            
+
+        min_val = float('inf')
+        for val in arr:
+            if val > num:
+                continue
+            result = 1 + self.least_count(num - val, arr, memo)
+            min_val = min(min_val, result)
+        
+
+        memo[num] = min_val
+        return memo[num]
 
 
 
@@ -71,10 +110,3 @@ class Solution:
 # 4
 # 20
 # 1
-
-
-
-                    if i in dp and (1 + dp[prev]) < dp[i]:
-                        dp[i] = 1 + dp[prev]
-                    else:
-                        dp[i] = 1 + dp[prev]
