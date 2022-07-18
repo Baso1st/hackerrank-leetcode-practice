0629055aedadd4a2bce8d1efe1b3378d98a0170a
Baso1st <abdulwanis1@gmail.com>
2022-07-18 14:46:20 -0400

    Solution to #235. Lowest Common Ancestor of a Binary Search Tree

---
+++ leetcode/lowest-ancestor-BST.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+#235. Lowest Common Ancestor of a Binary Search Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        node = root
+        while node:
+            if p.val < node.val and q.val < node.val:
+                node = node.left
+            elif p.val > node.val and q.val > node.val:
+                node = node.right
+            else:
+                return node
